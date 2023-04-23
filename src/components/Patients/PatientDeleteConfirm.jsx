c1391d03d3442e8d9081cc6707bec74ac63b1d39
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-23 20:31:31 +0500

    fixed a bug

--- src/components/Patients/PatientDeleteConfirm.jsx
+++ src/components/Patients/PatientDeleteConfirm.jsx
@@ -2,29 +2,44 @@ import { useQueryClient } from '@tanstack/react-query';
 import { collection, deleteDoc, doc } from 'firebase/firestore';
 import React, { useState } from 'react'
 import { auth, db } from '../Utils/firebase';
-import { EmailAuthProvider, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 import { toast } from 'react-toastify';
 
-export default function PatientDeleteConfirm({ hidePatientDeleteHandler,id }) {
+export default function PatientDeleteConfirm({ hidePatientDeleteHandler, id }) {
   const [password, setPassword] = useState(null);
   const queryClient = useQueryClient();
+  const [loading, setLoading] = useState(false)
 
   async function deletePatient() {
-    const user = queryClient.getQueryData(['user']);
-    try {
-      let credential=EmailAuthProvider.credential(user.email, password);
-      auth.currentUser
-      console.log(credential)
-      return
-      // const result = await signInWithEmailAndPassword(auth, user.email, password);
-      const patientRef = doc(collection(db, "patients"), id);
-      await deleteDoc(patientRef);
-      queryClient.invalidateQueries(['patients']);
-      toast.success("Patient Deleted")
-    } catch (error) {
-      console.log(error)
-      toast.error("Error deleting patient")
-    }
+    setLoading(true);
+    const user = auth.currentUser;
+    console.log(user);
+
+    const credential = EmailAuthProvider.credential(
+      user.email,
+      password
+    );
+
+    reauthenticateWithCredential(auth.currentUser, credential)
+      .then(async () => {
+        const patientRef = doc(collection(db, "patients"), id);
+        await deleteDoc(patientRef);
+        queryClient.invalidateQueries(['patients']);
+        toast.success("Patient Deleted");
+        setLoading(false);
+        hidePatientDeleteHandler();
+      })
+      .catch((error) => {
+        console.log(error.code)
+        if (error.code === "auth/wrong-password") {
+          toast.error("Wrong password");
+          setLoading(false);
+          return
+        }
+        toast.error("Error deleting patient")
+        setLoading(false);
+
+      });
   }
   return (
     <div onClick={() => hidePatientDeleteHandler()} className='w-[100vw] h-[100vh] bg-[rgba(0,0,0,0.2)] backdrop-blur-sm absolute z-[1000] top-0 left-0 flex justify-center items-center'>
@@ -35,8 +50,12 @@ export default function PatientDeleteConfirm({ hidePatientDeleteHandler,id }) {
           <input onChange={(e) => setPassword(e.target.value)} className='outline border-[2px] h-10 p-2 border-[rgba(0,0,0,0.1)] rounded-sm w-[100%]' placeholder='Password' />
           <div className='space-x-2'>
             <button onClick={() => hidePatientDeleteHandler()} className='w-24 h-10 rounded-sm bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:bg-gradient-to-l text-[18px] text-white'>Cancel</button>
-            <button onClick={() => { deletePatient(); }} className='w-24 h-10 border-2 border-[#AE89A5] text-[18px] text-[#AE89A5] hover:bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:text-white'>Confirm</button>
-          </div>
+            {loading?
+            <button className='w-24 max-w-[96px] h-10 relative top-[6px] border-2 border-[#AE89A5] text-[18px] text-[#AE89A5] hover:bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:text-white'>{<img className='w-[25px] m-auto' src='/WhiteLoading.svg'/>}</button>
+            :
+            <button onClick={() => { deletePatient(); }} className='w-24 max-w-[96px] h-10 border-2 border-[#AE89A5] text-[18px] text-[#AE89A5] hover:bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:text-white'>{"Confirm"}</button>
+            }
+            </div>
         </div>
       </div>
     </div>

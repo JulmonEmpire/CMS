0e2e51f0985f395701655638a89b26ca56b76a4f
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-07-08 20:40:21 +0500

    fix

--- src/components/Utils/firebase.js
+++ src/components/Utils/firebase.js
@@ -6,13 +6,22 @@ import { getStorage } from "firebase/storage";
 
 
 // Your web app's Firebase configuration
+// const app = initializeApp({
+//   apiKey: "AIzaSyBsBEM598X7899DKGoeAS5bAruDtW3eriU",
+//   authDomain: "mcp-ms.firebaseapp.com",
+//   projectId: "mcp-ms",
+//   storageBucket: "mcp-ms.appspot.com",
+//   messagingSenderId: "56471561180",
+//   appId: "1:56471561180:web:c2b2875d27693ed6362519"
+// });
 const app = initializeApp({
-  apiKey: "AIzaSyBsBEM598X7899DKGoeAS5bAruDtW3eriU",
-  authDomain: "mcp-ms.firebaseapp.com",
-  projectId: "mcp-ms",
-  storageBucket: "mcp-ms.appspot.com",
-  messagingSenderId: "56471561180",
-  appId: "1:56471561180:web:c2b2875d27693ed6362519"
+  apiKey: "AIzaSyCzW7fmAKcdbYphUrs8mzMCqVhRFyFMmC0",
+  authDomain: "mamojaki-m.firebaseapp.com",
+  projectId: "mamojaki-m",
+  storageBucket: "mamojaki-m.appspot.com",
+  messagingSenderId: "749066552580",
+  appId: "1:749066552580:web:22a5a8699b34b8072efc51",
+  measurementId: "G-NG7417P3YY"
 });
 const auth = getAuth(app);
 const db = getFirestore(app);

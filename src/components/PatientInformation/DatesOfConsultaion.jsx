b039c24c3a280d2fb6a4150b3c4ba5460f8a8e0d
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-08-02 14:07:48 +0500

    fix

--- src/components/PatientInformation/DatesOfConsultaion.jsx
+++ src/components/PatientInformation/DatesOfConsultaion.jsx
@@ -6,12 +6,14 @@ import AddDateModal from './AddDateModal';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { doc, updateDoc } from 'firebase/firestore';
 import { db } from '../Utils/firebase';
+import {AiFillDelete} from 'react-icons/ai'
+import {IoMdAlert} from "react-icons/io"
 
 export default function DatesOfConsultaion() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const queryClient=useQueryClient()
+  const queryClient = useQueryClient()
   const [showDateModal, setShowDateModal] = useState(false);
 
   const showAddModal = () => {
@@ -74,27 +76,30 @@ export default function DatesOfConsultaion() {
             const formattedDate = dateObj.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 
             // Calculate 6 years ago from today
-            const sixYearsAgo = new Date();
-            sixYearsAgo.setFullYear(sixYearsAgo.getFullYear() - 6);
-
             // const sixYearsAgo = new Date();
-            // sixYearsAgo.setDate(sixYearsAgo.getDate() - 1);
+            // sixYearsAgo.setFullYear(sixYearsAgo.getFullYear() - 6);
+
+            const sixYearsAgo = new Date();
+            sixYearsAgo.setDate(sixYearsAgo.getDate() - 1);
 
             // Check if the date is before 6 years ago
             const isDateBefore6YearsAgo = d < sixYearsAgo;
             return (
-              <div key={index} className='flex gap-10 justify-between items-center'>
-                <div onClick={() => navigate("/patients-information/notes", { state: { ...location.state, showAddButton: true, dateId: date.id, date: date.date } })} className='flex gap-10 cursor-pointer hover:opacity-70'>
-                  <div className='flex gap-4'>
-                    <p className='text-lg font-[500]'>{`${index + 1}.`}</p>
-                    {/* <p className='text-lg'>{`${d.toString().slice(0, 24)}`}</p> */}
-                    <p className='text-lg'>{`${d.toString().slice(0, 15)} ${formattedDate}`}</p>
+              <div key={index} className='flex gap-4 justify-between items-center'>
+                <>
+                  {isDateBefore6YearsAgo && (
+                    <IoMdAlert title='The last date of consultation was added 6 years ago. Please take necessary action' className='w-[20px] text-[#6C526F] h-[20px] rounded-full'></IoMdAlert>
+                  )}
+                  <div onClick={() => navigate("/patients-information/notes", { state: { ...location.state, showAddButton: true, dateId: date.id, date: date.date } })} className='flex flex-1 gap-10 cursor-pointer hover:opacity-70'>
+                    <div className='flex gap-4'>
+                      <p className='text-lg font-[500]'>{`${index + 1}.`}</p>
+                      {/* <p className='text-lg'>{`${d.toString().slice(0, 24)}`}</p> */}
+                      <p className='text-lg w-[240px]'>{`${d.toString().slice(0, 15)} ${formattedDate}`}</p>
+                    </div>
+                    <p>{date?.therapyType}</p>
                   </div>
-                  <p>{date?.therapyType}</p>
-                </div>
-                {isDateBefore6YearsAgo && (
-                  <button className='cursor-pointer flex justify-center items-center w-24 h-8 rounded-sm bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:bg-gradient-to-l text-lg text-white' onClick={(e) => { deleteDateOfConsultation(location.state.id, date.id); e.stopPropagation() }}>Delete</button>
-                )}
+                  <button className='cursor-pointer flex justify-center items-center p-4 py-2 rounded-sm bg-gradient-to-r from-[#6C526F] to-[#AE89A5] hover:bg-gradient-to-l text-lg text-white' onClick={(e) => { deleteDateOfConsultation(location.state.id, date.id); e.stopPropagation() }}><AiFillDelete/></button>
+                </>
               </div>
             )
           })}
